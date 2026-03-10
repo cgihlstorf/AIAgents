639016d60e4063887c383404f3f29722bc3fab3f
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2026-03-10 18:18:05 -0400

    start answet to exercise a discussion questions

--- Topic7MCP/exercise_a.py
+++ Topic7MCP/exercise_a.py
@@ -1,6 +1,23 @@
 import requests, os
 import json
 
+########## Answers to Discussion Questions ##########
+
+# 1. Which tool would you use to find all papers about "transformer attention mechanisms"? 
+# I would use the 'search_papers_by_relevance' tool, as it searches for papers based on the keywords in the input.
+
+# 2. Which would you use to find who else published in the same area as a specific author?
+# There are two approaches I could use: First, I could use is choosing a few papers by the authors that are representative of their research area
+# and, for each paper, using the 'get_citations' tool to get the papers that cite the paper (assuming these papers will
+# be in a similar research area). I could then use the 'get_paper' tool to get the authors from each paper (if author 
+# information is provided by the tool and if the paper id is available). 
+# Second, I could first use the 'get_author_papers' tool to get the papers written by the author to get a sense of 
+# what the author's research area is. Then I would use the 'search_papers_by_relevance' tool to do a keyword search 
+# for similar papers in that area and for each paper found, I could either manually inspect who the authors are or I 
+# could use the 'get_paper' tool if the paper id is provided to get the names of the authors of that paper, if that 
+# information is available via this tool call.
+
+
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/json, text/event-stream",
@@ -28,22 +45,44 @@ resp_dict = json.loads(resp_str)
 
 tools = resp_dict["result"]["tools"]
 
-print("==================== Tools ====================")
-
 for tool in tools:
     name = tool["name"]
-    description = tool["description"]
+    description = tool["description"].split("Args:")[0].strip()
 
-    print("Tool Name:", name)
-    print()
-    print("Tool Description:", description)
-    print()
-    print("Required Parameters:")
-    
-    required_params = tool["inputSchema"]["required"]
+
+    print("Tool:", name)
+    print("  Description:", description)
+
     properties = tool["inputSchema"]["properties"]
-    for r in required_params:
-        r_type = properties[r]["type"]
-        print(r + ":", "Type", r_type)
+    required_params = tool["inputSchema"]["required"]
+    required_str = ""
 
-    print("==" * 60)
+    for r in required_params:
+
+        r_type = properties[r]["type"]
+
+        required_str += r + f" ({r_type})"
+        
+        if required_params.index(r) != len(required_params) - 1: #don't include a comma for the last item in the list
+            required_str += ", "
+
+    all_params = list(properties.keys())
+
+    print("  Required:", required_str)
+
+    optional_str = ""
+
+    for o in all_params:
+
+        if o not in required_params: #skip required params
+
+            o_type = properties[o]["type"]
+
+            optional_str += o + f" ({o_type})"
+
+            if all_params.index(o) != len(all_params) - 1:
+                optional_str += ", "
+
+    print("  Optional:", optional_str)
+
+    print("\n")
